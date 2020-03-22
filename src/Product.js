f0d10f7f5f77bc4ae490aac5913e377ab8f9feb5
Redzuan <rabdulr@icloud.com>
2020-03-22 16:59:56 -0700

    Minor update to product page

--- src/Product.js
+++ src/Product.js
@@ -6,9 +6,9 @@ const Product = ({id, product, lineItem }) => {
             <h1>{ product.name }</h1>
             <img src={ product.image } />
             <h3>Description: { product.description }</h3>
-            <h3>Quantity: { lineItem ? lineItem.quantity : 0 }</h3>
+            <h3>Quantity: { lineItem ? lineItem.quantity : '0' }</h3>
         </div>
     )
 };
 
-export default Product
+export default Product;
