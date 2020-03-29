78e788c826c1ff094afcde4b8c3bf6a704d8f25d
Micah <mhirsch144@gmail.com>
2020-03-29 16:01:53 -0700

    added Admin.js component

---
+++ src/Admin.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Admin = ({ auth }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        axios.get('/api/users', {
+            headers: {
+                authorization: window.localStorage.getItem('token')
+            }
+        })
+            .then(response => console.log(response.data));
+    }, []);
+    return (
+        <div>
+            <h1>Admin</h1>
+            <a href='#'>Home</a>
+            <div>
+                <h2>Users</h2>
+                <ul>
+                    {
+                        users.map(user => {
+                            return (
+                                <li key={user.id}>
+                                    {user.username}
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+        </div>
+
+    );
+};
+
+export default Admin;
