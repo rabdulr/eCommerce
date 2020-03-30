ff2cbc31cd4a968fd7a7bb6ec53cf7bc6c65d9e9
Micah <mhirsch144@gmail.com>
2020-03-29 17:43:04 -0700

    add admin view to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Products from './Products';
 import Product from './Product';
 import User from './User';
 import Reset from './Reset';
+import Admin from './Admin';
 
 const headers = () => {
   const token = window.localStorage.getItem('token');
@@ -19,33 +20,33 @@ const headers = () => {
   };
 };
 
-const App = ()=> {
-  const [ params, setParams ] = useState(qs.parse(window.location.hash.slice(1)));
-  const [ auth, setAuth ] = useState({});
-  const [ orders, setOrders ] = useState([]);
-  const [ cart, setCart ] = useState({});
-  const [ products, setProducts ] = useState([]);
-  const [ lineItems, setLineItems ] = useState([]);
-  const [ cartQuantity, setCartQuantity ] = useState(0);
+const App = () => {
+  const [params, setParams] = useState(qs.parse(window.location.hash.slice(1)));
+  const [auth, setAuth] = useState({});
+  const [orders, setOrders] = useState([]);
+  const [cart, setCart] = useState({});
+  const [products, setProducts] = useState([]);
+  const [lineItems, setLineItems] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   useEffect(() => {
     axios.get('/api/products')
       .then(response => setProducts(response.data));
   }, []);
 
-  useEffect(()=>{
-    if(lineItems.length && cart.id){
-      setCartQuantity(lineItems.reduce((acc, item)=>{
-        if(item.orderId === cart.id){
-          acc = acc + item.quantity*1;
+  useEffect(() => {
+    if (lineItems.length && cart.id) {
+      setCartQuantity(lineItems.reduce((acc, item) => {
+        if (item.orderId === cart.id) {
+          acc = acc + item.quantity * 1;
           return acc;
         } else {
           return acc;
         }
-      }, 0 ));
+      }, 0));
     }
-  },[lineItems, cart]);
-  
+  }, [lineItems, cart]);
+
   useEffect(() => {
     if (auth.id) {
       const token = window.localStorage.getItem('token');
@@ -59,9 +60,9 @@ const App = ()=> {
   useEffect(() => {
     if (auth.id) {
       axios.get('/api/getCart', headers())
-      .then( response => {
-        setCart(response.data);
-      });
+        .then(response => {
+          setCart(response.data);
+        });
     }
   }, [auth]);
 
@@ -77,8 +78,8 @@ const App = ()=> {
   const createUserAccount = async (credentials) => {
     const created = (await axios.post('/api/createUserAccount', credentials)).data;
     login(credentials)
-      .then( () => window.location.hash='#')
-      .catch(ex=>console.log(ex));
+      .then(() => window.location.hash = '#')
+      .catch(ex => console.log(ex));
     ;
   };
 
@@ -111,16 +112,16 @@ const App = ()=> {
 
   const createOrder = () => {
     const token = window.localStorage.getItem('token');
-    axios.post('/api/createOrder', null , headers())
-    .then( response => {
-      setOrders([response.data, ...orders]);
-      const token = window.localStorage.getItem('token');
-      return axios.get('/api/getCart', headers())
-    })
-    .then( response => {
-      setCart(response.data);
-      setCartQuantity(0);
-    });
+    axios.post('/api/createOrder', null, headers())
+      .then(response => {
+        setOrders([response.data, ...orders]);
+        const token = window.localStorage.getItem('token');
+        return axios.get('/api/getCart', headers())
+      })
+      .then(response => {
+        setCart(response.data);
+        setCartQuantity(0);
+      });
     window.location.hash = '#';
   };
 
@@ -132,46 +133,46 @@ const App = ()=> {
       .catch(ex => console.log(ex))
   }
 
-  const addToCart = (productId, num)=> {
-      axios.post('/api/addToCart', { productId, num }, headers())
-      .then( response => {
+  const addToCart = (productId, num) => {
+    axios.post('/api/addToCart', { productId, num }, headers())
+      .then(response => {
         const lineItem = response.data;
-        const found = lineItems.find( _lineItem => _lineItem.id === lineItem.id);
-        const itemQuantity = lineItems.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item)=>{
-          acc = acc + item.quantity*1;
+        const found = lineItems.find(_lineItem => _lineItem.id === lineItem.id);
+        const itemQuantity = lineItems.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item) => {
+          acc = acc + item.quantity * 1;
           return acc;
-        }, 0 )
-        if(!found){
-          setLineItems([...lineItems, lineItem ])
+        }, 0)
+        if (!found) {
+          setLineItems([...lineItems, lineItem])
           setCartQuantity(itemQuantity + lineItem.quantity)
         }
         else {
           const updated = lineItems.map(_lineItem => _lineItem.id === lineItem.id ? lineItem : _lineItem);
           setLineItems(updated);
-          setCartQuantity(updated.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item)=>{
-            acc = acc + item.quantity*1;
+          setCartQuantity(updated.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item) => {
+            acc = acc + item.quantity * 1;
             return acc;
-          }, 0 ));
+          }, 0));
         }
       });
   };
-  const removeFromCart = (lineItem)=> {
+  const removeFromCart = (lineItem) => {
     axios.delete(`/api/removeFromCart/${lineItem.id}`, headers())
-    .then( () => {
-      const itemQuantity = lineItems.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item)=>{
-        acc = acc + item.quantity*1;
-        return acc;
-      }, 0 );
-      const currentCart = lineItems.filter(_lineItem => _lineItem.id !== lineItem.id );
-      setLineItems(currentCart);
-      setCartQuantity(itemQuantity - lineItem.quantity);
-    });
+      .then(() => {
+        const itemQuantity = lineItems.filter(_lineItems => _lineItems.orderId === lineItem.orderId).reduce((acc, item) => {
+          acc = acc + item.quantity * 1;
+          return acc;
+        }, 0);
+        const currentCart = lineItems.filter(_lineItem => _lineItem.id !== lineItem.id);
+        setLineItems(currentCart);
+        setCartQuantity(itemQuantity - lineItem.quantity);
+      });
   };
 
   const resetPassword = async (credentials) => {
     const { password, newPass } = credentials
     const token = (await axios.post('/api/auth', { username: auth.username, password })).data.token;
-    return (await axios.put(`/api/users/${auth.id}`, { userId: auth.id, newPass}, headers()));
+    return (await axios.put(`/api/users/${auth.id}`, { userId: auth.id, newPass }, headers()));
   };
 
   const { view, id } = params;
@@ -180,7 +181,7 @@ const App = ()=> {
     return (
       <div>
         {
-          !view && 
+          !view &&
           <Login login={login} />
         }
         {
@@ -195,46 +196,51 @@ const App = ()=> {
     return (
       <div>
         <a href='#'>
-        <h1>Foo, Bar, Bazz.. etc Store</h1>
+          <h1>Foo, Bar, Bazz.. etc Store</h1>
         </a>
         <h4>
           <a href='#view=cart'>
-            Total items in cart: { cartQuantity }
+            Total items in cart: {cartQuantity}
           </a>
           <br />
           <a href='#view=user'>
             User
           </a>
+          <br />
+          {auth.role === 'ADMIN' && <a href={`#view=admin`}>ADMIN</a>}
         </h4>
-        <button onClick={ logout }>Logout { auth.firstName } { auth.lastName } </button>
+        <button onClick={logout}>Logout {auth.firstName} {auth.lastName} </button>
         {
-          view === 'user' && 
+          view === 'user' &&
           <div>
-            <User userInfo = {auth} resetPassword={ resetPassword } />
-            <Orders lineItems={ lineItems } products={ products } orders={ orders } removeOrder={ removeOrder } />
+            <User userInfo={auth} resetPassword={resetPassword} />
+            <Orders lineItems={lineItems} products={products} orders={orders} removeOrder={removeOrder} />
           </div>
         }
         {
           view === 'reset' &&
-            <Reset  userInfo = {auth} resetPassword={ resetPassword } />
+          <Reset userInfo={auth} resetPassword={resetPassword} />
         }
-        { !view && 
+        {!view &&
           <div className='horizontal'>
-            <Products addToCart={ addToCart } products={ products } />
+            <Products addToCart={addToCart} products={products} />
 
           </div>
         }
         {
-          view === 'cart' && <Cart lineItems={ lineItems } removeFromCart={ removeFromCart } cart={ cart } createOrder={ createOrder } products={ products }/>
+          view === 'cart' && <Cart lineItems={lineItems} removeFromCart={removeFromCart} cart={cart} createOrder={createOrder} products={products} />
         }
-          {
-            view === 'product' && 
-              <Product
-                product={ products.find(product => product.id === id)}
-                lineItem={ lineItems.find(lineItem => lineItem.cartId === cart.id )}
-                addToCart = { addToCart }
-              />
-          }
+        {
+          view === 'product' &&
+          <Product
+            product={products.find(product => product.id === id)}
+            lineItem={lineItems.find(lineItem => lineItem.cartId === cart.id)}
+            addToCart={addToCart}
+          />
+        }
+        {
+          view === 'admin' && <Admin></Admin>
+        }
       </div>
     );
   }
